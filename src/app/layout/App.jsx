e1bf5657937f3db6b375e8be96c368d6ca2b8505
StephenDK <stephendklein08@gmail.com>
2020-03-16 20:13:11 -0700

    NavBar and Styles

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import './App.css';
 
+//Components
+import EventsDashboard from '../features/event/EventDashboard/EventsDashboard'
+import NavBar from '../features/nav/NavBar/NavBar';
+import Container from 'semantic-ui-react/dist/commonjs/elements/Container/Container';
+
 class App extends Component {
   render() {
-      return (
-        <div className="App">
-          <h1>Re-Vents</h1>
-        </div>
-      );
-    }
+    return (
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventsDashboard />
+        </Container>
+      </Fragment>
+    );
   }
+}
 
 export default App;
 
