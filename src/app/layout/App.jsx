1e992dd77fe5ee2890b3db9cb4831b0cc61c8f03
Stephen Klein <stephendklein08@gmail.com>
2020-04-12 18:42:27 -0700

    React Router DOM Typescript

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -3,7 +3,7 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 
 //Components
-import EventsDashboard from '../features/event/EventDashboard/EventsDashboard'
+import EventsDashboard from '../features/events/EventDashboard/EventsDashboard'
 import NavBar from '../features/nav/NavBar/NavBar';
 import Container from 'semantic-ui-react/dist/commonjs/elements/Container/Container';
 
