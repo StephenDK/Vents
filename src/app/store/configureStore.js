6c3f223a70b1c72843fbb062ff06241ccf1b812e
Stephen Klein <stephendklein08@gmail.com>
2020-04-15 17:49:25 -0700

    Redux DevTools

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,5 +1,11 @@
-import { createStore } from "redux"
+import { createStore } from "redux";
+
 //import testReducer from '../features/testarea/testReducer';
+
+
+// 5.0 setting up the devToolEnhancer for a better development experience
+// import devToolEnhancer and add to createStore as second param
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 //4.3 import rootReducer and replace testReducer with
 // rootReducer
@@ -14,7 +20,7 @@ export const configureStore = () => {
     // const store = createStore(testReducer);
     // 1.8 connect store to appliation using <Provider> ...See index.js
 
-    const store = createStore(rootReducer);  
+    const store = createStore(rootReducer, devToolsEnhancer());  
     // 4.4 Since the testReducer is accessed throught test now
     // we have to change mapStateToProps function in testComponent
     // data: state.test.data
