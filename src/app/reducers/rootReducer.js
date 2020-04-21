6b091403bc4250d8660e23cc971375601f6f4c66
Stephen Klein <stephendklein08@gmail.com>
2020-04-20 23:09:57 -0700

    Modals and Redux

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -5,16 +5,19 @@ import { combineReducers } from "redux";
 import testReducer from '../features/testarea/testReducer';
 import eventReducer from "../features/events/eventReducer";
 
+
 // Chapter 9.0 Adding Redux forms
 // To use redux from first import redux form reducer and add to root reducer
 // Then head over to the form where you want to configure
 import { reducer as FormReducer } from 'redux-form';
+import modalReducer from "../features/modals/modalReducer";
 
 // 4.1 to use the testReducer we can just call test
 const rootReducer = combineReducers({
     form: FormReducer,
     test: testReducer,
-    events: eventReducer
+    events: eventReducer,
+    modals: modalReducer
 })
 
 
