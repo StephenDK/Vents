9f51e267bcf940d4c2e05bbc88b756228f2eb371
Stephen Klein <stephendklein08@gmail.com>
2020-05-03 13:38:14 -0700

    Firebase Auth Login

--- src/app/features/auth/authActions.js
+++ src/app/features/auth/authActions.js
@@ -1,17 +1,21 @@
-import { LOGIN_USER, SIGN_OUT_USER } from "./authConstants";
+import { SIGN_OUT_USER } from "./authConstants";
 import { closeModal } from "../modals/modalActions";
 
 // 12.31 we refactored the code below to allow for async operations
 // since we will fetch user from database
 export const login = (creds) => {
-  return (dispatch) => {
-    dispatch({
-      type: LOGIN_USER,
-      payload: {
-        creds,
-      },
-    });
-    dispatch(closeModal())
+  // 15.0 Firebase login setup.
+  // we need to use firebase option with redux thunk
+  // the authentication is stored with firebase not firestore
+  return async (dispatch, getState, {getFirebase}) => {
+    // with this firebase const below we have access to firebase methods
+    const firebase = getFirebase();
+    try {
+      await firebase.auth().signInWithEmailAndPassword(creds.email, creds.password)
+      dispatch(closeModal())
+    } catch (error) {
+      console.log(error)
+    }
   };
 };
 // 12.32 lastly we need to hide the createEvent and people button 
