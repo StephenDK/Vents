81e1bc944edaf0d75304320454acb72ee679edcb
Stephen Klein <stephendklein08@gmail.com>
2020-05-10 18:18:15 -0700

    Radio Input Component

--- src/app/features/user/Settings/BasicPage.jsx
+++ src/app/features/user/Settings/BasicPage.jsx
@@ -1,53 +1,86 @@
 // 16.1 this is the start of the basics page
 // first we want to bring in the user data to display in the form
 // 16.2 head to SettingsDashboard.jsx to pass user data into this component
-import React, {Component} from 'react';
-import {Segment, Form, Header, Divider, Button} from 'semantic-ui-react';
-import {Field, reduxForm} from 'redux-form';
+import React, { Component } from "react";
+import { Segment, Form, Header, Divider, Button } from "semantic-ui-react";
+import { Field, reduxForm } from "redux-form";
 import DateInput from "../../../common/form/DateInput";
 import PlaceInput from "../../../common/form/PlaceInput";
 import TextInput from "../../../common/form/TextInput";
 
-class BasicPage extends Component {
+// 16.8 import the RadioButton we just created
+import RadioInput from "../../../common/form/RadioInput";
 
-    render() {
-        const {pristine, submitting} = this.props;
-        return (
-            <Segment>
-                <Header dividing size='large' content='Basics' />
-                <Form>
-                    <Field
-                        width={8}
-                        name='displayName'
-                        type='text'
-                        component={TextInput}
-                        placeholder='Known As'
-                    />
-                    <Form.Group inline>
-                      {/* todo: Gender Radio button */}
-                    </Form.Group>
-                    <Field
-                        width={8}
-                        name='dateOfBirth'
-                        component={DateInput}
-                        placeholder='Date of Birth'
-                    />
-                    <Field
-                        name='city'
-                        placeholder='Home Town'
-                        options={{types: ['(cities)']}}
-                        label='Female'
-                        component={PlaceInput}
-                        width={8}
-                    />
-                    <Divider/>
-                    <Button disabled={pristine || submitting} size='large' positive content='Update Profile'/>
-                </Form>
-            </Segment>
-        );
-    }
+// 16.9 Date picker setup
+// import addYears
+import { addYears } from "date-fns";
+
+class BasicPage extends Component {
+  render() {
+    const { pristine, submitting } = this.props;
+    return (
+      <Segment>
+        <Header dividing size="large" content="Basics" />
+        <Form>
+          <Field
+            width={8}
+            name="displayName"
+            type="text"
+            component={TextInput}
+            placeholder="Known As"
+          />
+          <Form.Group inline>
+            <label>Gender: </label>
+            <Field
+              name="gender"
+              type="radio"
+              value="male"
+              label="Male"
+              component={RadioInput}
+            />
+            <Field
+              name="gender"
+              type="radio"
+              value="female"
+              label="Female"
+              component={RadioInput}
+            />
+          </Form.Group>
+          <Field
+            width={8}
+            name="dateOfBirth"
+            component={DateInput}
+            placeholder="Date of Birth"
+            dateFormat="dd LL yyyy"
+            showYearDropdown={true}
+            showMonthDropdown={true}
+            dropdownMode="select"
+            maxDate={addYears(new Date(), -18)}
+          />
+          <Field
+            name="city"
+            placeholder="Home Town"
+            options={{ types: ["(cities)"] }}
+            label="Female"
+            component={PlaceInput}
+            width={8}
+          />
+          <Divider />
+          <Button
+            disabled={pristine || submitting}
+            size="large"
+            positive
+            content="Update Profile"
+          />
+        </Form>
+      </Segment>
+    );
+  }
 }
 // 16.5 this component wont render the user data as placeholder text when it loads
 // we must enable enableReinitialize : true in conecting this component to
 // redux form
-export default reduxForm({form: 'userProfile', enableReinitialize: true})(BasicPage);
+// 16.6 now lets create the radio buttons for gender. Head to Form folder/RadioInput.jsx
+export default reduxForm({ form: "userProfile", enableReinitialize: true })(
+  BasicPage
+);
