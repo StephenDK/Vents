ef9107bfa6d0feed713a27a7831e054114f1d75c
StephenDK <stephendklein08@gmail.com>
2020-06-09 21:30:48 -0700

    React Cropper

---
+++ src/app/features/user/Settings/Photos/CropperInput.jsx
@@ -0,0 +1,46 @@
+
+//17.12 We begin to bring in the react-cropper module so we can
+// let the user crop the photos they input
+// boilerplate component found at: https://github.com/roadmanfong/react-cropper
+import React, { Component, createRef } from 'react';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
+
+
+class CropperInput extends Component {
+    cropper = createRef();
+
+    cropImage = () => {
+        const {setImage} = this.props;
+        if (typeof this.cropper.current.getCroppedCanvas() === 'undefined') {
+            return;
+        }
+        this.cropper.current.getCroppedCanvas().toBlob(blob => {
+            console.log(blob);
+            setImage(blob)
+        }, 'image/jpeg')
+    }
+
+    render() {
+        const { imagePreview } = this.props;
+        return (
+            <Cropper
+                ref={this.cropper}
+                src={imagePreview}
+                style={{ height: 200, width: '100%' }}
+                preview='.img-preview'
+                // 17.13 We have to add another peice of state to hold the newly cropped image
+                // head over to the imagepage.jsx
+                aspectRatio={1}
+                viewMode={1}
+                drageMode='move'
+                guides={false}
+                scalable={true}
+                cropBoxMovable={true}
+                cropBoxResizable={true}
+                crop={this.cropImage} />
+        );
+    }
+}
+
+export default CropperInput;
